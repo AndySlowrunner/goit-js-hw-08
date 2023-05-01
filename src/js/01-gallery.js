121ba1d4836534b17a3dfa397526136976d222b6
Andy <andyfartushniy@gmail.com>
2023-05-01 20:36:38 +0300

    Added 1, 2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+            </li>`
+        })
+        .join("");
+};
+
+let lightbox = new SimpleLightbox('.gallery__link', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+const currentTimeKey = "videoplayer-current-time";
+
+
+const onUpdate = function (data) {
+  const saveTimeValue = JSON.stringify(data);
+  localStorage.setItem(currentTimeKey, saveTimeValue);
+
+};
+
+player.on('timeupdate', throttle(onUpdate, 1000));
+
+const playUpdateVideo = function() {
+  const savedTime = JSON.parse(localStorage.getItem(currentTimeKey));
+  if (savedTime === null) {
+    return;
+  }
+  const playTimeValue = savedTime.seconds;
+  if (playTimeValue) {
+    player.setCurrentTime(playTimeValue)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'Error':
+            break;
+          default:
+            break;
+        }
+      });
+  }
+};
+playUpdateVideo();
