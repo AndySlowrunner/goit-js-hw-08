d7c3ac9a65121ed5481b2f89678e96d0f8e9f956
Andy <andyfartushniy@gmail.com>
2023-05-03 15:33:36 +0300

    Changed 2 and 3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,7 +4,6 @@ const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input[name="email"]');
 const textarea = document.querySelector('textarea[name="message"]');
 const STORAGE_KEY = "feedback-form-state";
-const formData = {};
 
 
 form.addEventListener('submit', onFormSubmit);
@@ -12,28 +11,26 @@ form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
     
     if (input.value === '' || textarea.value === '') {
         return alert('Please fill in all fields!');
     };
     
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 };
 
 function onFormInput(evt) {
+    const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
     formData[evt.target.name] = evt.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
 function populateForm() {
-    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-    if (savedMessage) {
-        const { email, message } = savedMessage;
-        input.value = email;
-        textarea.value = message;
-    }
+    const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+    const { email, message } = formData;
+    input.value = email || '';
+    textarea.value = message || '';
 };
 populateForm();
